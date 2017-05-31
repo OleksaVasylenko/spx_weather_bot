f1d4c0b133cda08b90c7c9b669c168779667e6e5
Oleksa Vasylenko <spiralix604@gmail.com>
2017-05-31 11:47:49 +0300

    fixed utils

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -9,25 +8,12 @@ class UserSettings:
         self.location = location
         self.lang = lang
         self.notification = {}
-=======
-class UserSettings:
-    def __init__(self, user_info, location=None, lang='en', notify=None):
-        self.info = user_info
-        self.location = location
-        self.lang = lang
-        self.notify = []
->>>>>>> 280e58f1e2796ffd182a24c89a0117eae85f5e08
 
     def set_location(self, location):
         self.location = location
 
-<<<<<<< HEAD
     def set_notification(self, time):
         self.notification.update(time)
-=======
-    def set_notify(self, time):
-        self.notify.append(time)
->>>>>>> 280e58f1e2796ffd182a24c89a0117eae85f5e08
 
     def set_lang(self, lang):
         self.lang = lang
